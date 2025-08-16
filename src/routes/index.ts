d8bc70511d7fe3345674fdf919d710c3ca09acda
aleenajillani7-eng <aleenajillani7@gmail.com>
2025-08-16 08:33:32 +0500

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,11 @@
 // src/routes/index.ts
 import { Hono } from "hono";
 
-// API routers
+// API routers (mentions removed)
 import { tweetRouter } from "./tweet.route";
-import { mentionRouter } from "./mention.route";
 import scheduleRouter from "./schedule.routes";
 import webhookRouter from "./webhook.routes";
 import { authRouter } from "./auth.route";
-import twitterRouter from "./twitter.route"; // ⬅️ NEW
 
 // VIEW routers
 import loginRouter from "./login.routes";
@@ -16,11 +14,9 @@ import dashboardRouter from "./dashboard.routes";
 // ---- API aggregator ----
 export const apiRouter = new Hono();
 apiRouter.route("/tweets", tweetRouter);
-apiRouter.route("/mentions", mentionRouter);
 apiRouter.route("/schedule", scheduleRouter);
 apiRouter.route("/webhook", webhookRouter);
 apiRouter.route("/auth", authRouter);
-apiRouter.route("/twitter", twitterRouter); // ⬅️ NEW
 
 // ---- VIEW aggregator ----
 export const viewRouter = new Hono();
